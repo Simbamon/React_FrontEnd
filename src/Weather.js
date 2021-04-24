de22d97e7f969e28373a4d1d93ec0a9a1b0fe2ba
Simbamon <sbhan9511@gmail.com>
2021-04-24 22:52:53 +0900

    Geolocation added

--- src/Weather.js
+++ src/Weather.js
@@ -6,7 +6,9 @@ export default class Weather extends Component {
         super()
         this.state = {
             weather: "Not yet gotten",
-            name: "Not yet gotten"
+            name: "Not yet gotten",
+            lat: "",
+            long: "",
         }
     }
     handleButtonClick = () => {
@@ -25,14 +27,36 @@ export default class Weather extends Component {
         })
     }
 
+    geolocation = () => {
+        if('geolocation' in navigator) {
+            console.log('geolocation available')
+            navigator.geolocation.getCurrentPosition(position => {
+                this.setState({
+                    lat: position.coords.latitude,
+                    long: position.coords.longitude,
+                })
+                console.log(this.state.lat)
+                console.log(this.state.long)
+
+            })
+        }
+        else{
+            console.log('geolocation not available')
+        }
+    }
+
     render() {
         return (
             <div>
                 <button onClick={this.handleButtonClick}>Get weather in Minneapolis</button>
                 <br />
                 <button onClick={this.handleButtonClick2}>Get name</button>
+                <br />
+                <button onClick={this.geolocation}>Get geo</button>
                 <h1>The weather in Minneapolis is: {this.state.weather}</h1>
                 <h1>My name is: {this.state.name}</h1>
+                <h1>Lat: {this.state.lat}</h1>
+                <h1>Long: {this.state.long}</h1>
             </div>
         )
     }
